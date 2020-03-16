e83bd329affeb5c3eab1d6caf599e52a3b3ed448
Mathieu Grauwin <mathieu.grauwin1@gmail.com>
2020-03-16 22:03:47 +0100

    First version header and footer

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,28 +1,26 @@
 <template>
   <div id="app">
-    <img width="25%" src="./assets/logo.png">
-    <HelloWorld msg="Hello Vue" />
+    <Header></Header>
+
+    <main role="main" class="container">
+      <Home></Home>
+    </main>
+
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld";
+import Header from "./components/modules/Header";
+import Footer from "./components/modules/Footer";
+import Home from "./components/Home";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    Home,
+    Footer,
+    Header
   }
 };
 </script>
-
-<style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
