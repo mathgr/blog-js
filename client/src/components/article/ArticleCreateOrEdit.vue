568122c175894f38d892a141aa045b37b40c7570
Mathieu Grauwin <mathieu.grauwin1@gmail.com>
2020-03-22 18:03:09 +0100

    Manage error on edit of article

--- client/src/components/article/ArticleCreateOrEdit.vue
+++ client/src/components/article/ArticleCreateOrEdit.vue
@@ -1,21 +1,30 @@
 <template>
-    <form @submit.prevent="createOrEdit">
-        <div class="form-group">
-            <label for="title">Titre</label>
-            <input v-model="title" type="text" :class="{ 'is-invalid': isTitleInvalid }" class="form-control form-control-lg" id="title" required>
-            <div class="invalid-feedback">
-                Le titre de l'article doit avoir une longueur minimum de 3 caractères
+    <div>
+        <h2>{{ createOrEditLabel }}</h2>
+        <form @submit.prevent="createOrEdit">
+            <div v-if="error" class="alert alert-danger" role="alert">
+                Une erreur s'est produite
+                <button v-on:click="error = false" type="button" class="close" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
             </div>
-        </div>
-        <div class="form-group">
-            <label for="content">Contenu</label>
-            <textarea v-model="content" :class="{ 'is-invalid': isContentInvalid }" class="form-control" id="content" rows="6" required></textarea>
-            <div class="invalid-feedback">
-                Le contenu de l'article doit avoir une longueur minimum de 10 caractères
+            <div class="form-group">
+                <label for="title">Titre</label>
+                <input v-model="title" type="text" :class="{ 'is-invalid': isTitleInvalid }" class="form-control form-control-lg" id="title" required>
+                <div class="invalid-feedback">
+                    Le titre de l'article doit avoir une longueur minimum de 3 caractères
+                </div>
             </div>
-        </div>
-        <button type="submit" class="btn btn-primary">Enregistrer</button>
-    </form>
+            <div class="form-group">
+                <label for="content">Contenu</label>
+                <textarea v-model="content" :class="{ 'is-invalid': isContentInvalid }" class="form-control" id="content" rows="6" required></textarea>
+                <div class="invalid-feedback">
+                    Le contenu de l'article doit avoir une longueur minimum de 10 caractères
+                </div>
+            </div>
+            <button type="submit" class="btn btn-primary">Enregistrer</button>
+        </form>
+    </div>
 </template>
 
 <script>
@@ -26,12 +35,22 @@
         props: {
             isEditing: Boolean,
         },
+        computed: {
+            createOrEditLabel: function () {
+                if (this.isEditing) {
+                    return 'Édition de l\'article';
+                }
+
+                return 'Création d\'un article';
+            }
+        },
         data() {
             return {
                 title: '',
                 isTitleInvalid: false,
                 content: '',
                 isContentInvalid: false,
+                error: false,
             }
         },
         created() {
@@ -41,7 +60,10 @@
                         this.title = response.data.title;
                         this.content = response.data.content;
                     })
-                    .catch(error => console.log(error));
+                    .catch(error => {
+                        this.error = true;
+                        console.log(error)
+                    });
             }
         },
         methods: {
@@ -65,13 +87,19 @@
                 axios.defaults.headers.common = {'Authorization': `Bearer ${this.$store.state.jwtFromRequest}`};
                 axios.put(`${this.$api_url}/articles/${this.$route.params.id}`, data)
                 .then(() => this.$router.push({name: 'my-articles'}))
-                .catch(error => console.log(error));
+                .catch(error => {
+                    this.error = true;
+                    console.log(error);
+                });
             },
             create: function (data) {
                 axios.defaults.headers.common = {'Authorization': `Bearer ${this.$store.state.jwtFromRequest}`};
                 axios.post(`${this.$api_url}/articles`, data)
                 .then(() => this.$router.push({name: 'my-articles'}))
-                .catch(error => console.log(error))
+                .catch(error => {
+                    this.error = true;
+                    console.log(error);
+                })
             },
             checkForm: function () {
                 this.isTitleInvalid = false;
